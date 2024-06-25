614385837732b725de18943b8de07872b0a64686
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-06-25 22:13:34 +0400

    add vercel json

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,6 +1,25 @@
+import styled from "styled-components";
 
 export default function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <MainDiv>
+      <ArrowDiv>
+        <div></div>
+      </ArrowDiv>
+    </MainDiv>
+  );
 }
+
+const MainDiv = styled.div`
+  width: 1078px;
+  background-color: #f9f9f9;
+  padding-top: 45px;
+  padding-left: 48px;
+`;
+
+const ArrowDiv = styled.div`
+  width: 40px;
+  height: 40px;
+  padding: 12px 16.8px 12px 14px;
+  background-color: #fff;
+`;
