e6b01e08e6629b070ec1853e70ee30c8d298a3b9
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-06-26 17:15:16 +0400

    add header and input component

---
+++ src/components/exprerienceComp/Header.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <HeaderDiv>
+      <ArrowDiv>
+        <img src="/Vector.jpg" />
+      </ArrowDiv>
+      <SecondDiv>
+        <HeaderTxt>გამოცდილება</HeaderTxt>
+        <HeaderTxt>2/3</HeaderTxt>
+      </SecondDiv>
+    </HeaderDiv>
+  );
+}
+
+
+const ArrowDiv = styled.div`
+  width: 40px;
+  height: 40px;
+  padding: 12px 16.8px 12px 14px;
+  background-color: #fff;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const HeaderDiv = styled.div`
+  display: flex;
+  gap: 61px;
+  height: 41px;
+`;
+
+const HeaderTxt = styled.h1`
+  font-size: 24px;
+  color: #1a1a1a;
+`;
+
+const SecondDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  width: -webkit-fill-available;
+  border-bottom: 1px solid #1a1a1a;
+`;
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,19 +1,20 @@
 import styled from "styled-components";
+import Header from "../components/exprerienceComp/Header";
+import Input from "../components/exprerienceComp/Input";
 
 export default function Experience() {
   return (
     <MainDiv>
-      <HeaderDiv>
-        <ArrowDiv>
-          <img src="/Vector.jpg" />
-        </ArrowDiv>
-        <SecondDiv>
-          <HeaderTxt>გამოცდილება</HeaderTxt>
-          <HeaderTxt>2/3</HeaderTxt>
-        </SecondDiv>
-      </HeaderDiv>
+      <Header />
       <section style={{ marginTop: "77px", paddingLeft: "102px" }}>
-        <Label></Label>
+        <div style={{ display: "flex", gap: "31px", flexDirection: "column" }}>
+          <Input labelTxt="თანამდებობა" errorTxt="მინუმუმ 2 სიმბოლო">
+            დეველოპერი, დიზაინერი, ა.შ.
+          </Input>
+          <Input labelTxt="თანამდებობა" errorTxt="მინუმუმ 2 სიმბოლო">
+            დამსაქმებელი
+          </Input>
+        </div>
       </section>
     </MainDiv>
   );
@@ -25,33 +26,3 @@ const MainDiv = styled.div`
   padding: 45px 149px 65px 48px;
   min-height: 100vh;
 `;
-
-const ArrowDiv = styled.div`
-  width: 40px;
-  height: 40px;
-  padding: 12px 16.8px 12px 14px;
-  background-color: #fff;
-  border-radius: 50%;
-  cursor: pointer;
-`;
-
-const HeaderDiv = styled.div`
-  display: flex;
-  gap: 61px;
-  height: 41px;
-`;
-
-const HeaderTxt = styled.h1`
-  font-size: 24px;
-  color: #1a1a1a;
-`;
-
-const SecondDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
-  width: -webkit-fill-available;
-  border-bottom: 1px solid #1a1a1a;
-`;
-
-const Label = styled.label``;
