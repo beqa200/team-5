371578eac6c1abbf58840f06046c0b67460551c9
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-06-27 19:33:11 +0400

    style: add PersonalInfo's design structure

--- src/pages/PersonalInfo.tsx
+++ src/pages/PersonalInfo.tsx
@@ -1,11 +1,47 @@
+import styled from "styled-components";
 import BackArrowButton from "../components/BackArrowButton";
 import PageNameDiv from "../components/PageNameDiv";
 
 export default function PersonalInfo() {
   return (
     <div>
-      <BackArrowButton />
-      <PageNameDiv pageName={"ᲞᲘᲠᲐᲓᲘ ᲘᲜᲤᲝ"} pageNumber={2}/>
+      <MainContainer>
+      <LeftDiv>
+        <BackArrowButton />
+        <ContentDiv>
+        <PageNameDiv pageName={"ᲞᲘᲠᲐᲓᲘ ᲘᲜᲤᲝ"} pageNumber={2}/>
+          
+        </ContentDiv>
+      </LeftDiv>
+      <RightComponent>CV</RightComponent>
+    </MainContainer>
     </div>
   )
 }
+
+const MainContainer = styled.div`
+  width: 100vw;
+  display: flex;
+`;
+
+const LeftDiv = styled.div`
+  width: 100%;
+  flex: 1;
+  background-color: #f9f9f9;
+  padding: 48px 130px 65px 48px;
+  display: flex;
+  align-items: flex-start;
+  gap: 60px;
+`;
+
+const RightComponent = styled.div`
+  width: 650px;
+  background-color: #ffffff;
+`;
+
+const ContentDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
