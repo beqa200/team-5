f18179d20ba700d11b37a0c7766a53c25ff0f3fa
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-06-29 00:42:42 +0400

    feat: add error colors to inputs

--- src/components/PersonalInfoForm.tsx
+++ src/components/PersonalInfoForm.tsx
@@ -2,71 +2,115 @@ import styled from 'styled-components'
 import { useForm } from 'react-hook-form';
 
 function PersonalInfoForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors, submitCount } } = useForm();
 
   const onSubmit = (data: unknown) => {
     console.log(data);
   };
 
+  const getBorderColor = (fieldName: string) => {
+    if(submitCount > 0){
+      if(errors[fieldName]){
+        return "red";
+      }else{
+        return "#98E37E";
+      }
+    } 
+    return "#BCBCBC";
+  };
+
   return (
     <Container onSubmit={handleSubmit(onSubmit)}>
       <NamesDiv>
-        <NameLeft errorsname={errors.name}>
+        <NameLeft>
           <label>სახელი</label>
-          <input type="text" placeholder='ჩაწერეთ სახელი' id="name" 
-           {...register('name', {
-             required: "სახელი სავალდებულოა",
-             minLength: {
-              value: 2,
-              message: 'მინიმუმ 2 ასო, ქართული ასოები'
-             }
-             })} 
-             />
-
-        <span>მინიმუმ 2 ასო, ქართული ასოები</span>
+          <input 
+            type="text" 
+            placeholder='ჩაწერეთ სახელი' 
+            id="name" 
+            style={{borderColor: getBorderColor('name')}}
+            {...register('name', {
+              required: "სახელი სავალდებულოა",
+              minLength: {
+                value: 2,
+                message: 'მინიმუმ 2 ასო, ქართული ასოები'
+              }
+            })} 
+          />
+          <span>მინიმუმ 2 ასო, ქართული ასოები</span>
         </NameLeft>
         <SurnameRight>
-        <label>გვარი</label>
-          <input type="text" placeholder='ჩაწერეთ გვარი' id="surname" {...register('surname', {
-            required: "გვარი სავალდებულოა",
-            minLength: {
-              value: 2,
-              message: 'მინიმუმ 2 ასო, ქართული ასოები'
-            }
-          })} />
+          <label>გვარი</label>
+          <input 
+            type="text" 
+            placeholder='ჩაწერეთ გვარი' 
+            id="surname" 
+            style={{borderColor: getBorderColor('surname')}}
+            {...register('surname', {
+              required: "გვარი სავალდებულოა",
+              minLength: {
+                value: 2,
+                message: 'მინიმუმ 2 ასო, ქართული ასოები'
+              }
+            })} 
+          />
           <span>მინიმუმ 2 ასო, ქართული ასოები</span>
         </SurnameRight>
       </NamesDiv>
 
-    <div style={{display: "flex" , justifyContent: "start", width: "100%", flexDirection: "column"}}>
+      <div style={{display: "flex" , justifyContent: "start", width: "100%", flexDirection: "column"}}>
         <UploadImage>
           <span>პირადი ფოტოს ატვირთვა</span>
           <input type="file" id="upload"/>
           <label htmlFor="upload">ატვირთვა</label>
         </UploadImage>
 
-      <TextareaDiv>
-        <span>ჩემ შესახებ (არასავალდებულო)</span>
-        <textarea name="" placeholder='ძალიან მიყვარს დიზაინის კეთება. დილით ადრე რომ ავდგები გამამხნევებელი ვარჯიშების მაგიერ დიზაინს ვაკეთებ.'></textarea>
-      </TextareaDiv>
-    </div>
+        <TextareaDiv>
+          <span>ჩემ შესახებ (არასავალდებულო)</span>
+          <textarea 
+            name="" 
+            placeholder='ძალიან მიყვარს დიზაინის კეთება. დილით ადრე რომ ავდგები გამამხნევებელი ვარჯიშების მაგიერ დიზაინს ვაკეთებ.'
+          ></textarea>
+        </TextareaDiv>
+      </div>
 
-    <EmailDiv>
-      <label>ელ.ფოსტა</label>
-      <input type="email" placeholder='ანზორი@gmail.com' />
-      <span>უნდა მთავრდებოდეს @redberry.ge-ით</span>
-    </EmailDiv>
+      <EmailDiv>
+        <label>ელ.ფოსტა</label>
+        <input 
+          type="email" 
+          placeholder='ანზორი@gmail.com' 
+          style={{borderColor: getBorderColor('email')}}
+          {...register('email', {
+            required: "ელ.ფოსტა სავალდებულოა",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@redberry\.ge$/,
+              message: "უნდა მთავრდებოდეს @redberry.ge-ით"
+            }
+          })}
+        />
+        <span>უნდა მთავრდებოდეს @redberry.ge-ით</span>
+      </EmailDiv>
 
-    <NumberDiv>
-      <label>მობილურის ნომერი</label>
-      <input type="number" placeholder='+995 597 63 45 16' />
-      <span>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</span>
-    </NumberDiv>
+      <NumberDiv>
+        <label>მობილურის ნომერი</label>
+        <input 
+          type="text" 
+          placeholder='+995 597 63 45 16' 
+          style={{borderColor: getBorderColor('number')}}
+          {...register('number', {
+            required: "მობილურის ნომერი სავალდებულოა",
+            pattern: {
+              value: /^\+995\s\d{3}\s\d{2}\s\d{2}\s\d{2}$/,
+              message: "უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს"
+            }
+          })}
+        />
+        <span>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</span>
+      </NumberDiv>
 
-    <div style={{display: "flex" , justifyContent: "end", width: "100%"}}>
-    <Button>შემდეგი</Button>
-    </div>
-
+      <div style={{display: "flex" , justifyContent: "end", width: "100%"}}>
+        <Button>შემდეგი</Button>
+      </div>
     </Container>
   )
 }
@@ -100,7 +144,7 @@ const NameLeft = styled.div`
     padding: 0.8125rem 0.875rem 0.875rem 1rem;
     border-radius: 0.25rem;
     outline: none;
-    border: {${props => props.errorsname ? "1px solid red" : "1px solid green"}}; 
+    border: 1px solid;
   }
   input::placeholder{
     color: #000;
@@ -120,37 +164,10 @@ const NameLeft = styled.div`
     opacity: 0.5;
   }
 `
-const SurnameRight = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  gap: 0.5rem;
-  width: 100%;
+const SurnameRight = styled(NameLeft)`
   padding-left: 1.5rem;
-
-    input{
-    padding: 0.8125rem 0.875rem 0.875rem 1rem;
-    border-radius: 0.25rem;
-    border: 1px solid #BCBCBC;
-  }
-  input::placeholder{
-    color: #000;
-    opacity: 0.6;
-    font-size: 1rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1.3125rem; 
-  }
-
-  span{
-    color: var(---500, #2E2E2E);
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 300;
-    line-height: 1.3125rem;
-    opacity: 0.5;
-  }
 `
+
 const UploadImage = styled.div`
   display: flex;
   margin-top: 2.69rem;
@@ -216,7 +233,8 @@ const EmailDiv = styled.div`
     align-items: center;
     align-self: stretch;
     border-radius: 0.25rem;
-    border: 1px solid #BCBCBC;
+    border: 1px solid;
+    outline: none;
   }
   span{
     color: var(---500, #2E2E2E);
@@ -227,21 +245,7 @@ const EmailDiv = styled.div`
     opacity: 0.5;
   }
 `
-const NumberDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 1.06rem;
-    gap: 0.5rem;
-    width: 100%;
-
-  input{
-    display: flex;
-    padding: 0.8125rem 5.1875rem 0.875rem 1rem;
-    align-items: center;
-    align-self: stretch;
-    border-radius: 0.25rem;
-    border: 1px solid #BCBCBC;
-  }
+const NumberDiv = styled(EmailDiv)`
   input[type=number]{
     opacity: 0.6;
   }
@@ -249,14 +253,6 @@ const NumberDiv = styled.div`
   input[type=number]::-webkit-inner-spin-button {
       -webkit-appearance: none;
       margin: 0;
-  }
-  span{
-    color: var(---500, #2E2E2E);
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 300;
-    line-height: 1.3125rem; 
-    opacity: 0.5;
   }
 `
 const Button = styled.button`
